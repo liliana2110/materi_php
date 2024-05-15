79df2a4b07c2bc48937f1526d0baf5e2895ff64d
liliana2110 <madelianadewi2110@gmail.com>
2024-05-15 19:00:54 +0800

    materi

---
+++ pratikum/pt2.php
@@ -0,0 +1,11 @@
+<?php
+echo "<h2>Nilai Mahasiswa : </h2>";
+$nim = '2301010321';
+$nama = 'Ni Made Liana  Dewi';
+$prodi = 'Manajemen Data dan Informasi';
+$nilai = 90;
+echo "NIM : ", $nim, "<br>";
+echo "Nama : ", $nama, "<br>";
+echo "Prodi : ", $prodi, "<br>";
+echo "Nilai : $nilai";
+?>
